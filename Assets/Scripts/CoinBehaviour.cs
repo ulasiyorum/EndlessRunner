427da2952a555ee24c27a42e1c7c3f256f1e2233
Ulaş Gergerli <20200808049@ogr.akdeniz.edu.tr>
2023-02-09 04:40:37 +0300

    Update CoinBehaviour.cs

--- Assets/Scripts/CoinBehaviour.cs
+++ Assets/Scripts/CoinBehaviour.cs
@@ -24,7 +24,7 @@ public class CoinBehaviour : MonoBehaviour
             int size = UnityEngine.Random.Range(0, 5);
             if(size == 4)
             {
-                size = UnityEngine.Random.Range(4, 8);
+                size = UnityEngine.Random.Range(2, 8);
             }
             linkedCoins = new LinkedList<GameObject>();
             linkedCoins.AddFirst(gameObject);
