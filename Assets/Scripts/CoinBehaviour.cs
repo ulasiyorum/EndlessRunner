96e7964b56e2e40c256a70cfdf973a099f369b7b
Ulaş Gergerli <20200808049@ogr.akdeniz.edu.tr>
2023-02-09 04:37:11 +0300

    add: Coins to spawn randomly p1

--- Assets/Scripts/CoinBehaviour.cs
+++ Assets/Scripts/CoinBehaviour.cs
@@ -5,15 +5,40 @@ using UnityEngine;
 public class CoinBehaviour : MonoBehaviour
 {
     [SerializeField] int point;
+    private static LinkedList<GameObject> linkedCoins; 
     void Start()
     {
-        
+        LinkCoins();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void LinkCoins()
+    {
+        if(linkedCoins == null)
+        {
+            int size = UnityEngine.Random.Range(0, 5);
+            if(size == 5)
+            {
+                size = UnityEngine.Random.Range(4, 10);
+            }
+            linkedCoins = new LinkedList<GameObject>();
+            linkedCoins.AddFirst(gameObject);
+
+            Debug.Log(size);
+            for(int i = 0; i < size; i++)
+            {
+                GameObject go = Instantiate(gameObject);
+                GameObject last = linkedCoins.Last.Value;
+                float scale = go.GetComponent<MeshCollider>().bounds.size.z * 12;
+                go.transform.position = new Vector3(transform.position.x, transform.position.y, last.transform.position.z + scale);
+                linkedCoins.AddLast(go);
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
